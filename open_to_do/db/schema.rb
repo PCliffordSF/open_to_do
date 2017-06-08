f3cac69a59ad417e48281ef22949d9181a1ed5ba
Paul Clifford <paulclifford2570@gmail.com>
2017-06-08 00:57:38 +0000

    added password attribute

--- open_to_do/db/schema.rb
+++ open_to_do/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608004010) do
+ActiveRecord::Schema.define(version: 20170608005653) do
 
   create_table "items", force: :cascade do |t|
     t.string   "to_do"
@@ -37,5 +37,7 @@ ActiveRecord::Schema.define(version: 20170608004010) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
+
 end
